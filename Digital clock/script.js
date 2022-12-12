5a07c0c88fd16ed1c84c8c70e980d3c393d3d4d0
NataliiaBoiko22 <nata.boyko.kras@gmail.com>
2022-12-12 22:52:01 +0100

    add files

---
+++ Digital clock/script.js
@@ -0,0 +1,30 @@
+let clock = document.querySelector(".clock");
+let currDate = document.querySelector(".date");
+
+function time() {
+  let date = new Date();
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  let month = months[date.getMonth()];
+  let day = date.getDate();
+  let hours = date.getHours() < 10 ? "0" + date.getHours() : date.getHours();
+  let minutes =
+    date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes();
+  let seconds =
+    date.getSeconds() < 10 ? "0" + date.getSeconds() : date.getSeconds();
+  currDate.innerHTML = `${month} ${day} `;
+  clock.innerHTML = `${hours}:${minutes}:${seconds}`;
+}
+setInterval(time, 1000);
